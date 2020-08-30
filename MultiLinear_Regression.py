ef88ae06aa948d7a88d7980697b482b668565c20
Ali Raza <engr.aliraza52@gmail.com>
2020-08-30 17:35:38 +0500

    features and theetas embeded to matrix form

--- MultiLinear_Regression.py
+++ MultiLinear_Regression.py
@@ -11,18 +11,32 @@ dataSet = pd.read_csv("C:/Users/Ali Raza/Google Drive/Logo Generator Project/Dat
 #Age,Sex,Survived features and predict fare
 ageDataX = dataSet.iloc[:,5]
 ageDataX = np.where(np.isnan(ageDataX), np.ma.array(ageDataX,mask = np.isnan(ageDataX)).mean(axis = 0), ageDataX)
+ageDataX = np.reshape(ageDataX,(-1,1))
 sexDataX = dataSet.iloc[:,4] #create dummy
 survivalDataX = dataSet.iloc[:,1] #dummy
+survivalDataX = np.asarray(survivalDataX,dtype = int)
+survivalDataX = np.reshape(survivalDataX,(-1,1))
 fareDataY = dataSet.iloc[:,9]
 #LabelEncoding
 lb = LabelEncoder()
 sexDataX = lb.fit_transform(sexDataX)
+sexDataX = np.reshape(sexDataX,(-1,1))
 
 #featureMatrix
-featureMatrix = np.concatenate()
-
+featureShape = ageDataX.shape
+featureMatrixRows = featureShape[0]
+residueColumn = np.ones((featureMatrixRows,1))
+featureMatrix = np.concatenate((residueColumn,ageDataX,sexDataX,survivalDataX),axis = 1)
 
 #---------------------------Functions------------------
+
+#theetaVector
+def theetaMatrix(totalFeatures):
+    theetaVectorArray = np.random.uniform(0,2,size = totalFeatures)
+    
+    return theetaVectorArray
+
+theetaVector = theetaMatrix(4)
 
 #Normalization
 def normalizationFunction(x):
@@ -48,8 +62,9 @@ def removeOutliers(x):
 
 #hypothesis
 def hypothesis(featureArray, theetaArray):
-    return featureArray*theetaArray    
-
+    newArray = featureArray*theetaArray    
+    return np.sum(newArray)
+    
 def partialDerivativeTheetaZero(featureArray,updatedTheetas):
     hypothesisArray = featureArray*updatedTheetas
     return  (hypothesisArray - fareDataY)/featureArrayRows  #Define ROws
